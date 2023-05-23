09f635012fbda05d1f447b141f5b0b0b9f193aa9
Hussam ahtash <Hussamahtash11@gmail.com>
2023-05-23 13:31:52 +0200

    pom

---
+++ src/test/java/com/example/efusionFactureThymeleaf/EfusionFactureThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.efusionFactureThymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfusionFactureThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
